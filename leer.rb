eb4e4387a6768c8a3cb24c481e79d6fe9c303d65
JohnTigerino <136547627+JohnTigerino@users.noreply.github.com>
2024-08-07 22:25:10 -0600

    Add files via upload

---
+++ leer.rb
@@ -0,0 +1,7 @@
+#puts "Ingrese su primer nombre"
+#nombre = gets
+#puts "Bienvenido "+nombre+" disfrute!"
+
+puts "Ingrese su primer nombre"
+nombre = gets.chomp
+puts "Bienvenido #{nombre} disfrute!"
